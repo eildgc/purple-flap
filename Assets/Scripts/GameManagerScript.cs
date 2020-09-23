519c8ff713cba64ec702e5b9334fdb57a5fc0b25
Tutz <tutz_26@hotmail.com>
2020-09-23 00:58:43 -0600

    Created basic game functionality

---
+++ Assets/Scripts/GameManagerScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManagerScript : MonoBehaviour
+{
+
+    public GameObject gameOverCanvas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 1;
+    }
+
+    // Game Over Screen
+    public void GameOver()
+    {
+        gameOverCanvas.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void Replay()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public GameManagerScript gameManager;
+    public float velocity = 1;
+    private Rigidbody2D rb;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        // Mouse COntroller
+        if(Input.GetMouseButtonDown(0))
+        {
+            //Jump
+            rb.velocity = Vector2.up * velocity;
+        }
+
+        // Touch Controller
+        if(Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if(touch.phase == TouchPhase.Began)
+            {
+                //Jump
+                rb.velocity = Vector2.up * velocity;
+            }
+
+        }
+
+
+    }
+
+    //Loosing!!!
+    private void onCollisionEnter2D(Collision2D collision)
+    {
+        gameManager.GameOver();
+    }
+
+
+}
